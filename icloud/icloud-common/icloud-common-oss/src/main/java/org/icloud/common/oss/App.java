f9382b802801fb27e4d1520b0f7efc29fc4f6a74
DESKTOP-0COF1JF\MAYUNKUN <1163363720@qq.com>
2018-08-14 14:57:08 +0800

    云项目首次提交

---
+++ icloud/icloud-common/icloud-common-oss/src/main/java/org/icloud/common/oss/App.java
@@ -0,0 +1,13 @@
+package org.icloud.common.oss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
